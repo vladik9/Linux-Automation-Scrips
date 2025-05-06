493c91bd39cfb2f492221a7444c2c794b259061c
Vladik <v.cornici@gmail.com>
2025-05-06 22:35:43 +0300

    fixed java script

--- scripts/java.bash
+++ scripts/java.bash
@@ -2,7 +2,10 @@
 echo "################################################"
 echo "Installing Java..."
 sudo apt-get install java-common
-sudo dpkg -i ../deb/java-21-amazon-corretto-jdk_21.0.4.7-1_amd64.deb
+sudo apt-get update;
+sudo apt-get install -y java-21-amazon-corretto-jdk
+sudo wget https://corretto.aws/downloads/latest/amazon-corretto-21-x64-linux-jdk.deb
+sudo dpkg -i ./amazon-corretto-21-x64-linux-jdk.deb
 java -version
 echo "Java has been installed successfully."
 echo "################################################"
